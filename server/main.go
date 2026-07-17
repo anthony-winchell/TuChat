54f211a0de2c3b8177617a1a9fac3c5be8880fe5
anthony-winchell <123203535+anthony-winchell@users.noreply.github.com>
2026-07-17 14:46:40 -0400

    big refactor of server. pulled out methods and attached them to their related structs. clients are now stored in a slice, setting up for broadcasting

--- server/main.go
+++ server/main.go
@@ -3,63 +3,105 @@ package main
 import (
 	"log"
 	"net"
-	"io"
 )
 
 type Client struct {
-	Username string
-	Conn     net.Conn
+	conn     net.Conn
+	username string
+}
+
+type Server struct {
+	listener net.Listener
+	clients  []*Client
+}
+
+func (s *Server) Start() {
+	defer s.listener.Close()
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		go s.handleConnection(conn)
+	}
+}
+
+func (s *Server) removeClient(client *Client) {
+	for i, c := range s.clients {
+		if c == client {
+			s.clients = append(s.clients[:i], s.clients[i+1:]...)
+			return
+		}
+	}
+}
+
+func (s *Server) handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	client, err := s.registerClient(conn)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	log.Printf("%s Connected", client.username)
+
+	s.handleMessages(client)
+
+	log.Printf("%s Disconnected", client.username)
+	
+	s.removeClient(client)
+}
+
+func (s *Server) registerClient(conn net.Conn) (*Client, error) {
+	if _, err := conn.Write([]byte("Connection Successful\n Enter Username:")); err != nil {
+		return nil, err
+	}
+
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	username := string(buffer[:n-1])
+
+	client:= &Client{
+		conn:     conn,
+		username: username,
+	}
+
+	s.clients = append(s.clients, client)
+
+	return client, nil
+}
+
+func (s *Server) handleMessages(client *Client) {
+	buffer := make([]byte, 1024)
+	for {
+		n, err := client.conn.Read(buffer)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		message := string(buffer[:n])
+		log.Printf("%s: %s", client.username, message)
+	}
 }
 
 func main() {
-	listener, err := net.Listen("tcp",":8080")
+	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Println(err)
-		return 
+		return
 	}
-	defer listener.Close()
-	log.Println("Listening on :8080")
 
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		go handleConnection(conn)
+	server := &Server{
+		listener: listener,
 	}
+
+	server.Start()
+
 }
-func handleConnection(conn net.Conn) {
-		defer conn.Close()
-
-		_, err := conn.Write([]byte("Connection Successful\n Enter Username: "))
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		buffer := make([]byte, 1024)
-		n, err := conn.Read(buffer)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		username := string(buffer[:n-1])
-		
-		log.Println("Username:", username)
-
-		for {
-			n, err := conn.Read(buffer)
-			if err != nil {
-				if err == io.EOF {
-					log.Println("Client Disconnected")
-					return 
-				}
-				log.Println(err)
-				return
-			}
-
-			message := string(buffer[:n])
-			log.Printf("%s: %s", username, message)
-		}
-	}
